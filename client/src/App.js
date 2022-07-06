0bab1bd6765d8296ab13cfc07426ad0e0323026e
Ezequiel Peralta <ezequiiel.peralta@gmail.com>
2022-07-06 18:48:06 -0300

    componenteDetail

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,13 @@ import React from 'react'
 import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter, Route} from 'react-router-dom'
+import MovieDetail from './Components/movieDetail';
 
 function App() {
   return (
-    <h1>andando</h1>
+    <BrowserRouter>
+    <Route exact path='/movies/:id' component={MovieDetail}/>
+    </BrowserRouter>
   );
 }
 
--- client/src/Redux/reducer/index.js
+++ client/src/Redux/reducer/index.js
@@ -1,3 +1,19 @@
- const rootReducer = () =>{}
+import { GET_MOVIE_DETAIL } from "../actions"
+
+const initialState={
+    movieDetail:{}
+}
+
+ const rootReducer = (state=initialState, action) =>{
+    switch(action.type){
+        case GET_MOVIE_DETAIL:
+            return{
+                ...state,
+                movieDetail:action.payload
+            }
+            default:
+                return state
+    }
+ }
 
  export default rootReducer
