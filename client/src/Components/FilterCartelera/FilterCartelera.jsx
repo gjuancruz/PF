f034848dbeb8428fd8af7bb388b5c0186a671f3d
bugaringonzalo <bugaringonzalo@gmail.com>
2022-07-07 16:08:55 -0300

    Bootstrap added to FilterCartelera

--- client/src/Components/FilterCartelera/FilterCartelera.jsx
+++ client/src/Components/FilterCartelera/FilterCartelera.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useDispatch } from 'react-redux';
-import { filterByType, filterByGenre } from "../Redux/actions";
+import { filterByType, filterByGenre } from "../../Redux/actions/index";
 
 
 export default function FilterCartelera () {
@@ -24,64 +24,33 @@ export default function FilterCartelera () {
     return (
         <div>
 
-          <div class="btn-group" role="group" aria-label="Button group with nested dropdown">
-            <button type="button" class="btn btn-primary">Primary</button>
-            <div class="btn-group" role="group">
-              <button id="btnGroupDrop1" type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></button>
-              <div class="dropdown-menu" aria-labelledby="btnGroupDrop1" style="will-change: transform;">
-                <a class="dropdown-item" href="#">Dropdown link</a>
-                <a class="dropdown-item" href="#">Dropdown link</a>
+          <div className="btn-group" role="group" aria-label="Button group with nested dropdown">
+            <button type="button" className="btn btn-primary">Select Genre</button>
+            <div className="btn-group" role="group">
+              <button id="btnGroupDrop1" type="button" className="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></button>
+              <div className="dropdown-menu" aria-labelledby="btnGroupDrop1" style={{'willChange': 'transform'}}>
+                <option className="dropdown-item" value="All" onClick={(e) => handleSelectGenre(e)}>All</option>
+                <option className="dropdown-item" value="Horror" onClick={(e) => handleSelectGenre(e)}>Horror</option>
+                <option className="dropdown-item" value="Thriller" onClick={(e) => handleSelectGenre(e)}>Thriller</option>
+                <option className="dropdown-item" value="Sports" onClick={(e) => handleSelectGenre(e)}>Sports</option>
+                <option className="dropdown-item" value="Science-Fiction" onClick={(e) => handleSelectGenre(e)}>Science-Fiction</option>
+                <option className="dropdown-item" value="Suspense" onClick={(e) => handleSelectGenre(e)}>Suspense</option>
               </div>
             </div>
           </div>
 
-          <div class="btn-group" role="group" aria-label="Button group with nested dropdown">
-            <button type="button" class="btn btn-success">Success</button>
-            <div class="btn-group" role="group">
-              <button id="btnGroupDrop2" type="button" class="btn btn-success dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></button>
-              <div class="dropdown-menu" aria-labelledby="btnGroupDrop2">
-                <a class="dropdown-item" href="#">Dropdown link</a>
-                <a class="dropdown-item" href="#">Dropdown link</a>
+          <div className="btn-group" role="group" aria-label="Button group with nested dropdown">
+            <button type="button" className="btn btn-success">Select Type</button>
+            <div className="btn-group" role="group">
+              <button id="btnGroupDrop2" type="button" className="btn btn-success dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></button>
+              <div className="dropdown-menu" aria-labelledby="btnGroupDrop2">
+                <option className="dropdown-item" value="All" onClick={(e) => handleSelectType(e)}>All</option>
+                <option className="dropdown-item" value="2-D" onClick={(e) => handleSelectType(e)}>2-D</option>
+                <option className="dropdown-item" value="3-D" onClick={(e) => handleSelectType(e)}>3-D</option>
+                <option className="dropdown-item" value="4-D" onClick={(e) => handleSelectType(e)}>4-D</option>
               </div>
             </div>
           </div>
-
-          <div class="btn-group" role="group" aria-label="Button group with nested dropdown">
-            <button type="button" class="btn btn-info">Info</button>
-            <div class="btn-group" role="group">
-              <button id="btnGroupDrop3" type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></button>
-              <div class="dropdown-menu" aria-labelledby="btnGroupDrop3">
-                <a class="dropdown-item" href="#">Dropdown link</a>
-                <a class="dropdown-item" href="#">Dropdown link</a>
-              </div>
-            </div>
-          </div>
-
-          <div class="btn-group" role="group" aria-label="Button group with nested dropdown">
-            <button type="button" class="btn btn-danger">Danger</button>
-            <div class="btn-group" role="group">
-              <button id="btnGroupDrop4" type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></button>
-              <div class="dropdown-menu" aria-labelledby="btnGroupDrop4">
-                <a class="dropdown-item" href="#">Dropdown link</a>
-                <a class="dropdown-item" href="#">Dropdown link</a>
-              </div>
-            </div>
-          </div>
-        
-            <select name="genre" onChange={(e) => handleSelectGenre(e)} >
-                <option value="All">All</option>
-                <option value='Horror'>Horror</option>
-                <option value='Thriller'>Thriller</option>
-                <option value='Sports'>Sports</option>
-                <option value='Science-Fiction'>Science-Fiction</option>
-                <option value='Suspense'>Suspense</option>
-            </select>
-            <select name="type" onChange={(e) => handleSelectType(e)} >
-                <option value="All">All</option>
-                <option value="2-D">2-D</option>
-                <option value="3-D">3-D</option>
-                <option value="4-D">4-D</option>
-            </select>
         </div>
     )
 }
