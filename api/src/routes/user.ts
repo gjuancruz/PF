a876586a2cfc68dcaa017cd159dafaaca5664911
IBrunello <86432849+IBrunello@users.noreply.github.com>
2022-07-07 15:36:01 -0300

    Rutas

--- api/src/routes/user.ts
+++ api/src/routes/user.ts
@@ -1,4 +1,5 @@
 import {Response, Request, Router, NextFunction} from 'express';
+import { ArrayTypeNode } from 'typescript';
 import {Movie} from '../models/Movie';
 // import {sequelize} from '../db'
 
@@ -12,22 +13,35 @@ const router = Router();
 //     res.send('soy la ruta post!');
 // });
 
-router.get('/', (req: Request, res: Response, next: NextFunction) => {
+router.get('/movie', (req: Request, res: Response, next: NextFunction) => {
     Movie.findAll()
      .then((movie:any) => {
+        let movielist : Object[] = movie.map((e:Object)=>{
+
+        })
       res.send(movie);
      })
      .catch((error:unknown) => res.send(error));
     //  .catch((error:unknown) => next(error));
 });
 
-router.post('/', (req: Request, res: Response, next: NextFunction) => {
-    const user = req.body;
-    console.log(user);
+router.get("/movie/:title", async(req:Request,res:Response,next:NextFunction)=>{
+    const title = req.params.title
+    try{
+        const movie = await Movie.findOne({where:{Title:title}})
+        res.send(movie)
+        }catch(err:unknown){
+        res.send(err)
+    }
+})
+
+router.post('/movie', (req: Request, res: Response, next: NextFunction) => {
+    const movie = req.body;
+    console.log(movie);
     
-    Movie.create(user)
-     .then((createdUser) => {
-      res.send(createdUser);
+    Movie.create(movie)
+     .then((createdMovie) => {
+      res.send(createdMovie);
      })
      .catch((error) => next(error));
 });
