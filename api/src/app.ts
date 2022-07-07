a27c83b684f20deed8ed1e1d22424da0b89a3977
unknown <luisdavid.0698@hotmail.com>
2022-07-07 17:08:27 -0500

    rutas back

--- api/src/app.ts
+++ api/src/app.ts
@@ -6,7 +6,7 @@ import cookieParser from 'cookie-parser';
 import morgan from 'morgan';
 
 // import config from '../lib/config';
-// import routes from './routes/index'
+import routes from './routes/index'
 
 const prisma = new PrismaClient();
 
@@ -39,14 +39,14 @@ app.use((err: error, req: Request, res: Response, next: NextFunction) => {
     res.status(status).send(message);
 });
 
-// app.use('/api', routes);
+app.use('/api', routes);
 
 
 //Traer todos los registros
-app.get('/movies', async(req: Request, res: Response) => {
-    const post = await prisma.movie.findMany();
-    res.send(post);
-});
+// app.get('/movies', async(req: Request, res: Response) => {
+//     const post = await prisma.movie.findMany();
+//     res.send(post);
+// });
 
 interface movies {
     Title: string;
@@ -62,32 +62,6 @@ interface movies {
     Runtime: number;
 }
 
-app.post("/", async (req:Request, res:Response) =>{
-    // console.log("CLICK")
-    // res.send("OK")
-    const {Title,Plot,Genre,Actors,Language,Director,Release,Poster,Rated,Trailer,Runtime} = req.body
-    const a = await prisma.movie.create({
-        data: {
-            Title,
-            Plot,
-            Genre,
-            Actors,
-            Language,
-            Director,
-            Release,
-            Poster,
-            Rated,
-            Trailer,
-            Runtime
-        },
-      })
-
-
-    // const allUsers = await prisma.product.findMany()
-    // console.log(allUsers)
-    res.json(a)
-
-})
 
 // crear registro db
 // app.post('/post', async(req: Request, res: Response):any => {
--- api/src/routes/movies.ts
+++ api/src/routes/movies.ts
@@ -1,12 +1,7 @@
-
 import {Router, Request, Response}  from 'express'
-// import { Op } from "sequelize"
-
 import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
-
-// const {Movie,Room} = require("../db");
 
 const router = Router()
 
@@ -16,20 +11,57 @@ let month = date.getMonth()
 let year = date.getFullYear()
 
 
-router.get("/billboard", async (req:Request, res:Response) =>{
+router.post("/", async (req:Request, res:Response) =>{
+    const {Title,Plot,Genre,Actors,Language,Director,Release,Poster,Rated,Trailer,Runtime} = req.body
+    const movie = await prisma.movie.create({
+        data: {
+            Title,
+            Plot,
+            Genre,
+            Actors,
+            Language,
+            Director,
+            Release,
+            Poster,
+            Rated,
+            Trailer,
+            Runtime
+        },
+      })
 
+    res.json(movie)
+
+})
+
+
+router.get("/billboard", async (req:Request, res:Response) =>{
+    
     try{
         const list = await prisma.movie.findMany({
-            where:{
-                Release:{}
-            }
+            // where:{
+                //     Release:{}
+            // }
         })
+        res.json(list)
+        // res.json(date)
 
     }catch (error) {
         res.status(404).json("No se obtuvieron datos")
     }
 })
 
+router.get("/:id", async (req:Request,res:Response) =>{
+    const {id} = req.params
+    try{
+        const movie = await prisma.movie.findUnique({
+            where:{id:id}
+        })
+        res.json(movie)
+
+    }catch(e){
+        res.status(404).json("no se encontró la movie")
+    }
+})
 
 
 // router.get("/billboard", async (req:Request, res:Response) =>{
