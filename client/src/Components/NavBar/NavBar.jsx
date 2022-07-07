a48ca92136dcdcd87da6b1aa16adb94d9c60d158
gjuancruz <tatugalaz@gmail.com>
2022-07-07 11:31:03 -0300

    carousel feature added and navbar update

--- client/src/Components/NavBar/NavBar.jsx
+++ client/src/Components/NavBar/NavBar.jsx
@@ -1,46 +1,50 @@
 import React from 'react'
 
+
 const NavBar = () =>{
 
     return(
-        <div>
-            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <a class="navbar-brand" href="#">
-    <img src="https://images.vexels.com/media/users/3/220740/isolated/preview/21dd8b736fd55ca78afad1913daa86f5-icono-de-trazo-de-entradas-de-cine-clasico.png" width="30" height="30" alt=""/>
-  </a>
-  <a class="navbar-brand" href="#">Henry Cinema</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Mi perfil</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Próximos estrenos</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Comida</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Promos</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Membresías</a>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </div>
-</nav>
+      <nav class="navbar navbar-expand-lg bg-dark text-light">
+      <div class="container-fluid">
+        <a class="navbar-brand text-light" href="/">Henry Cinema</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active text-light" aria-current="page" href="/">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-light" href="/">Próximos estrenos</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-light" href="/">Comida</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-light" href="/">Promos</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle text-light" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Usuario
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li><a class="dropdown-item" href="#">Mi perfil</a></li>
+                <li><a class="dropdown-item" href="#">Mis comentarios</a></li>
+                <li><a class="dropdown-item" href="#">Membresía</a></li>
+                <li><hr class="dropdown-divider"/></li>
+                <li><a class="dropdown-item" href="#">Cerrar sesión</a></li>
+              </ul>
+            </li>
+            
+          </ul>
+          <form class="d-inline-flex" role="search">
+            <input class="form-control me-2" type="search" placeholder="Search..." aria-label="Search"/>
+            <button class="btn btn-warning" type="submit">Search</button>
+          </form>
         </div>
+      </div>
+    </nav>
     )
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -5,14 +5,18 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from "react-redux";
 import {store} from './Redux/store/index'
+import { ChakraProvider } from '@chakra-ui/react'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+  <ChakraProvider>
+    <React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </React.StrictMode>
+  </ChakraProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
