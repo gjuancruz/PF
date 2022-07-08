ab1cf3b262db7f86d8f689da2215a9294a1d2ba6
bugaringonzalo <bugaringonzalo@gmail.com>
2022-07-08 17:19:02 -0300

    CreateMovie in progress

--- client/src/Components/CreateMovie/CreateMovie.jsx
+++ client/src/Components/CreateMovie/CreateMovie.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { useForm } from "react-hook-form";
 
 const CreateMovie = () => {
     
--- client/src/Redux/actions/index.js
+++ client/src/Redux/actions/index.js
@@ -2,23 +2,6 @@
 
 export const FILTER_TYPE = 'FILTER_TYPE';
 export const FILTER_GENRE = 'FILTER_GENRE';
-export const GET_MOVIE_DETAIL='GET_MOVIE_DETAIL';
-const Apikey='8de7320'
-
-
-export function getMovieDetail(idMovie){
-    return  function(dispatch){
-            fetch(`https://www.omdbapi.com/?apikey=${Apikey}&i=${idMovie}`)
-            .then(response=>response.json())
-            .then(res=>{
-                dispatch({
-                    type:GET_MOVIE_DETAIL,
-                    payload:res
-                })
-            })
-    }
-    
-}
 
 export function filterByType (payload) {
     return {
