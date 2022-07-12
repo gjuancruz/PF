9d2673d31af7156910006f5bfb794a172cc7bde7
EEA94 <14elias06andrada94@gmail.com>
2022-07-12 19:10:38 -0300

    probando subir a dashboard

--- api/src/app.ts
+++ api/src/app.ts
@@ -15,7 +15,6 @@ app.use(express.urlencoded({extended: true, limit: '50mb'})); //middleware
 app.use(express.json({limit: '50mb'}));
 app.use(cookieParser());
 app.use(morgan('dev'));
-app.use(express.static("../../client"))
 
 app.use(
     cors({
