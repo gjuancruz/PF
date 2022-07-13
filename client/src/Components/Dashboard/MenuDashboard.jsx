6bb54d810f11766777a12cd5eb2946607b080539
EEA94 <14elias06andrada94@gmail.com>
2022-07-13 20:13:17 -0300

    asdasd

--- client/src/Components/Dashboard/MenuDashboard.jsx
+++ client/src/Components/Dashboard/MenuDashboard.jsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 import SideBar from "./SideBar";
+import Modal from "../reusable/Modal"
+import Users from "./Users";
+
 
 export default function MenuDashboard(){
+    const [active, setActive] = useState(false);
+
+    const toggle = ()=>{
+      setActive(!active);
+    }
+
     return (
         <>
+        <button onClick={toggle}>pueba modal</button>
+        <Modal active={active} toggle={toggle}>
+            <h4>Probando modal</h4>
+        </Modal>
         <SideBar/>
+        <Users/>
         </>
     )
 }
