c1c5e6194765e37d84cbeb91e56f2ea802c64f05
gjuancruz <tatugalaz@gmail.com>
2022-07-13 11:53:07 -0300

    login form

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import NavBar from './Components/NavBar/NavBar';
 import Footer from './Components/Footer/Footer';
 import Home from './Components/Home/Home';
 import CreateMovie from './Components/CreateMovie/CreateMovie';
+import Login from './Components/Login/Login';
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <Route exact path='/' component={Home} />
         <Route exact path='/movies/:id' component={MovieDetail}/>
         <Route exact path= '/create' component={CreateMovie} />
+        <Route exact path = '/login' component={Login} />
         <Route path='/' component={Footer} />
     </BrowserRouter>
   );
---
+++ client/src/Components/Login/Login.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { useState } from "react";
+import styles from './Login.module.css';
+
+const Login = () => {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleSubmit = (event) =>{
+        event.preventDefault()
+    }
+    return (
+        <div className="container">
+            <form className={styles.formLogin}>
+                <h1 className="h3 mb-3 font-weight-normal">Log In</h1>
+                <label for="inputEmail" className="sr-only">Email de Usuario</label>
+                <input type="email" id="inputEmail" className="form-control mb-2" placeholder="Email address" required="" autofocus="" value={email} onChange={(event) => setEmail(event.target.value)}/>
+                <label for="inputPassword" className="sr-only" >Contraseña</label>
+                <input type="password" id="inputPassword" className="form-control mb-2" placeholder="Password" required="" value={password} onChange={(event) => setPassword(event.target.value)}/>
+                <div className="checkbox mb-3">
+                    <label>
+                        <input type="checkbox" value="remember-me" /> Recordarme
+                    </label>
+                </div>
+                <button className="btn btn-lg btn-warning btn-block" type="submit">Ingresar</button>
+                <p className="mt-5 mb-3 text-muted">© Moon Cinema - 2022 </p>
+            </form>
+
+            {/* <form className={styles.formLogin}>
+                <div className="text-center mb-4">
+                    <h1 className="h3 mb-3 font-weight-normal">Sign In</h1>
+                </div>
+
+                <div className="form-label-group mb-3">
+                    <label for="inputEmail">Email</label>
+                    <input type="email" id="inputEmail" className="form-control" placeholder="Correo electrónico" required="" autofocus="" />
+                </div>
+
+                <div className="form-label-group mb-3">
+                    <label for="inputPassword">Contraseña</label>
+                    <input type="password" id="inputPassword" className="form-control" placeholder="Contraseña" required="" />
+                </div>
+
+                <div className="checkbox mb-3">
+                    <label>
+                    <input type="checkbox" value="remember-me" /> Recordarme
+                    </label>
+                </div>
+                <button className="btn btn-lg btn-warning btn-block" type="submit">Ingresar</button>
+                <p className="mt-5 mb-3 text-muted text-center">© Moon Cinema - 2022</p>
+                </form> */}
+
+        </div>
+    );
+
+}
+
+export default Login;
