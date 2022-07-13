90d2235d91a87156851581dbad21a97243067e02
unknown <luisdavid.0698@hotmail.com>
2022-07-12 22:14:25 -0500

    BackRoutes--comments-feedback

--- api/index.ts
+++ api/index.ts
@@ -7,12 +7,12 @@ const prisma = new PrismaClient()
 
 app.listen(3001, async () => {
 
-//    const del1 = await prisma.comment.deleteMany({})
-//    const del = await prisma.movie.deleteMany({})
+   const del1 = await prisma.comment.deleteMany({})
+   const del = await prisma.movie.deleteMany({})
 
-//    const movie = await prisma.movie.createMany({   
-//       data: [cars, spider, sonic, iceAge, thor, jurassic, MinionsTheRiseofGru, lightyear, topGun, DrStranger, Minions, MinionsHolidaySpecial, SupermanSpidermanorBatman]
-//   })
+   const movie = await prisma.movie.createMany({   
+      data: [cars, spider, sonic, iceAge, thor, jurassic, MinionsTheRiseofGru, lightyear, topGun, DrStranger, Minions, MinionsHolidaySpecial, SupermanSpidermanorBatman]
+  })
    console.log(`Server ready at: http://localhost:3001`);
 })
 
