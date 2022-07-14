24ac091b215ca09385c0feb70de6f73df79a8605
unknown <luisdavid.0698@hotmail.com>
2022-07-14 15:38:34 -0500

    Admin comments front

--- client/src/Components/Dashboard/MenuDashboard.jsx
+++ client/src/Components/Dashboard/MenuDashboard.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Comments from "./Comments";
 import SideBar from "./SideBar";
 
 export default function MenuDashboard(){
     return (
-        <>
-        <SideBar/>
-        </>
+        <div class="row bg-dark">          
+            <div class="col-sm-4"> <SideBar/> </div>
+            <div class="col-sm-8"> <Comments/> </div>
+        </div>
     )
 }
