d7da4bc06db2cb201529dacf778c94e73b776af9
IBrunello <86432849+IBrunello@users.noreply.github.com>
2022-07-15 16:29:55 -0300

    modelos permanentes

--- api/index.ts
+++ api/index.ts
@@ -10,6 +10,9 @@ const movielist : any =  [cars, spider, sonic, iceAge, thor, jurassic, MinionsTh
 export const showGenerator = async(show:any) => {
 
    const data = []
+
+   data.push(show)
+   console.log(data)
    
    const movie = await prisma.movie.findUnique({where:{id:show.movieId}})
    const time = movie?.Runtime
@@ -18,47 +21,45 @@ export const showGenerator = async(show:any) => {
    const hour = time ? Math.floor(time/60): 13
    const minute = time ? time % 60 : 0
    
+   const max = 1440
+   const num = Math.floor(time ? max/time : 5)
    
-   for(let i = 0;i<540;time ? i+=time : i+= 50){
+   for(let i = 0;i<num;i++){
       let last = data.reverse().find((e:any)=>e.movieId==show.movieId)
       data.reverse()
       const lasthour = parseInt(last ? last.schedule.slice(0,2):"13")
       const lastminute = parseInt(last ? last.schedule.slice(3,5):"00")
       
       var newhour = lasthour+hour
-      var newminute = lastminute + minute
-      
+      var newminute = lastminute + minute + 10 
       if(newminute>=60) {
          newhour+=1
          newminute %= 60
       }
+      
+      if(newhour!<24){
       if(newminute<10) {data.push({schedule:newhour+":0"+newminute,movieId:show.movieId,roomId:show.roomId}) 
       }
       else {data.push({schedule:newhour+":"+newminute,movieId:show.movieId,roomId:show.roomId}) 
       }
+   }else{return data}
    }
    return data
 }
 
 app.listen(3001, async () => {
 
-   const del1 = await prisma.comment.deleteMany({})
-   const del2 = await prisma.show.deleteMany({})
+   // const del1 = await prisma.comment.deleteMany({})
+   // const del2 = await prisma.show.deleteMany({})
    const del3 = await prisma.room.deleteMany({})
-   const del = await prisma.movie.deleteMany({})
+   // const del = await prisma.movie.deleteMany({})
 
-   const movies = await prisma.movie.createMany({   
-      data: movielist
-  })
-
-  const movie : any = await prisma.movie.findMany({where:{id!:undefined}})
-
-  const index : any= movie.map((e:any)=>{
-   return{
-      id:movie.indexOf(e)
-   }
-  })
-
+   for(let i = 0;i<movielist.length;i++){
+   const movies = await prisma.movie.upsert({   
+      where:{Title:movielist[i].Title},
+      update:{},
+      create:movielist[i]
+  })}
   const rooms = await prisma.room.createMany({
    data:[{
       id:1
@@ -73,22 +74,22 @@ app.listen(3001, async () => {
       id:5
    }]
   })
+  
+//   const movie : any = await prisma.movie.findMany({where:{id!:undefined}})
 
-  const room : any= await prisma.room.findMany({where:{id!:undefined},select:{id:true}})
+//   const index : any= movie.map((e:any)=>{
+//    return{
+//       id:movie.indexOf(e)
+//    }
+//   })
 
-  var data = room.map((e:any)=>{
-   return{
-   schedule:"13:00",
-   movieId:movie[room.indexOf(e)].id,
-   roomId:e.id
-}})
+//   const room : any= await prisma.room.findMany({where:{id!:undefined},select:{id:true}})
 
-const showdata = (await showGenerator(data[0])).concat(await showGenerator(data[1])).concat(await showGenerator(data[2])).concat(await showGenerator(data[3])).concat(await showGenerator(data[4]))
-const newdata = data.concat(showdata)
-
-
-  const show = await prisma.show.createMany({
-   data: newdata
-  })
+//   var data = room.map((e:any)=>{
+//    return{
+//    schedule:"13:00",
+//    movieId:movie[room.indexOf(e)].id,
+//    roomId:e.id
+// }})
    console.log(`Server ready at: http://localhost:3001`);
 })
