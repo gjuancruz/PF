409625902c2e5050d36fe34622066edf59f641b4
EduDan343 <dan_999@hotmail.es>
2022-07-16 17:50:47 -0500

    modificacion para deployar...

--- api/index.ts
+++ api/index.ts
@@ -1,11 +1,14 @@
 // import { sequelize } from './src/db';
+require('dotenv').config();
 import app from './src/app';
 import {PrismaClient} from '@prisma/client'
 import {cars, spider, sonic, iceAge, thor, jurassic, MinionsTheRiseofGru, lightyear, topGun, DrStranger, Minions, MinionsHolidaySpecial, SupermanSpidermanorBatman} from "./src/routes/data"
 
 const prisma = new PrismaClient()
 
-app.listen(3001, async () => {
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, async () => {
 
    const del1 = await prisma.comment.deleteMany({})
    const del = await prisma.movie.deleteMany({})
