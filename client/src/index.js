5acbf6804e184c45fdab942f90e2e7d0a5ad9f9f
EduDan343 <dan_999@hotmail.es>
2022-07-16 21:08:34 -0500

    intento redeploy quitando dotenv

--- client/src/index.js
+++ client/src/index.js
@@ -6,12 +6,12 @@ import reportWebVitals from './reportWebVitals';
 import { Provider } from "react-redux";
 import {store} from './Redux/store/index'
 import axios from 'axios';
-import dotenv from 'dotenv'
-dotenv.config();
+// import dotenv from 'dotenv'
+// dotenv.config();
 
 console.log(process.env)
 
-axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
+axios.defaults.baseURL = process.env.REACT_APP_API;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
