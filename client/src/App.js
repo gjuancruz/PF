d08c50ee9270695a7adb5c0cbd6530854fa8eff6
LautiOcampo <lautiocampo@hotmail.es>
2022-07-20 11:08:38 -0300

    inicio candy

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import Error404 from './Components/Error404/Error404';
 import CreateMovie from './Components/CreateMovie/CreateMovie';
 import Register from './Components/Register/Register';
 import Perfil from './Components/Profile/Profile';
+import Candy from './Components/Prueba Candy/PruebaCandy';
 import { Redirect } from 'react-router-dom';
 
 const stripePromise = loadStripe("pk_test_51LKmPfJSzK67IevuPbWPySSkTZkbIKMk89qalSxH06sTUPsU8UeojNRIT11QhjW7yheUsm4BBjtkYc2jD6Q9O8KQ00IWWYeOtN")
@@ -38,6 +39,7 @@ function App() {
         <Route exact path="/feedback" component={CreateFeedback}/>
         <Route exact path='/profile' component={Perfil}/>
         <Route exact path='/register' component={Register}/>
+        <Route exact path='/candy' component={Candy}/>
         <Route path='*' component={Error404} />
       </Switch>
     </BrowserRouter>
--- client/src/Components/CreateFeedback/PostFeedback.jsx
+++ client/src/Components/CreateFeedback/PostFeedback.jsx
@@ -4,12 +4,6 @@ import { postFeedback, getUsers } from "../../Redux/actions";
 
 export default function CreateFeedback(props) {
   const dispatch = useDispatch();
-<<<<<<< HEAD
-  // console.log(props);
-  // var idUser = props.match.params.id;
-  // console.log("Este es el user id:", idUser);
-=======
->>>>>>> ca135f68cc4cb645c5f6b794adbc5b6af0427ba2
   const [input, setInput] = useState({
     Text: "",
   });
@@ -40,47 +34,6 @@ const allUsers = useSelector ((state) => state.usuarios)
   }
 
   return (
-<<<<<<< HEAD
-    <>
-      <button
-        type="button"
-        class="btn btn-primary"
-        data-bs-toggle="modal"
-        data-bs-target="#exampleModal"
-      >
-        Launch demo modal
-      </button>
-
-      <div
-        class="modal fade"
-        id="exampleModal"
-        tabindex="-1"
-        aria-labelledby="exampleModalLabel"
-        aria-hidden="true"
-      >
-        <div class="modal-dialog modal-dialog-centered ">
-          <div class="modal-content bg-dark ">
-            <div class="modal-header">
-              <h4 class="modal-title" id="exampleModalLabel">
-                Dejar un feedback
-              </h4>
-              {/* <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button> */}
-            </div>
-            <div class="modal-body">
-              <div class="container-fluid">
-                <div class="row">
-                  <div class="col-4">
-                    <h5>Tu feedback: </h5>
-                  </div>
-                  <div class="col">
-                    <input
-                      type="text"
-                      value={input.Text}
-                      name="Text"
-                      style={{ width: "100%" }}
-                      onChange={(e) => handleChange(e)}
-                    />
-=======
     <div>
 
       {
@@ -127,7 +80,6 @@ const allUsers = useSelector ((state) => state.usuarios)
                         />
                       </div>
                     </div>
->>>>>>> ca135f68cc4cb645c5f6b794adbc5b6af0427ba2
                   </div>
                 </div>
                 <div class="modal-footer">
