db5101131007dbe33681ddc14dd0657423fbf6e6
Ezequiel Peralta <ezequiiel.peralta@gmail.com>
2022-07-23 01:20:02 -0300

    change detail and home

--- client/src/Components/Detail/MovieDetail.jsx
+++ client/src/Components/Detail/MovieDetail.jsx
@@ -219,7 +219,7 @@ Comprar
                 Cerrar
               </button>
               <button type="button" class="btn btn-warning" >
-                Ir al Carrito
+                Agregar al carrito
               </button>
             </div>
           </div>
