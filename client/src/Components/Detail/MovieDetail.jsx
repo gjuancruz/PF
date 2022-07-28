e13540a4173d6966252f70f48314ea2ab4b8a188
Ezequiel Peralta <ezequiiel.peralta@gmail.com>
2022-07-28 11:34:18 -0300

    cambios

--- client/src/Components/Detail/MovieDetail.jsx
+++ client/src/Components/Detail/MovieDetail.jsx
@@ -32,6 +32,7 @@ export default function MovieDetail(){
     const currentUser = allUser.filter((u) => u.id === userIdCheck);
     const movieVideo=useSelector(state=>state.movieDetail.Trailer);
     const [checkbtn, setcheckbtn] = useState(true);
+    const userCarrito = useSelector(state => state.userCart)
     
     console.log("soy el wacho",shows)
     // console.log("es la premier",allCartelera)
@@ -256,7 +257,7 @@ export default function MovieDetail(){
             data-bs-toggle="modal" data-bs-target="#staticBackdropcheck"
             disabled={num === 0}
             onClick={()=> setcheckbtn((e)=> !e)}
-            > <p className="numbuton"> {num === 0 ? "": num}</p>
+            >    
             <svg xmlns="http://www.w3.org/2000/svg" width="40" height="70" fill="currentColor" class="bi bi-cart2" viewBox="0 0 16 16">
   <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l1.25 5h8.22l1.25-5H3.14zM5 13a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"/>
 </svg>
